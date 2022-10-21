bdcc0c524f36a25c5e5eba975de67e871ea6b3bb
28drinks <28kadsen@gmail.com>
2022-10-21 02:11:19 +0200

    db update 21/10

--- get_share_values.py
+++ get_share_values.py
@@ -177,9 +177,9 @@ if __name__ == '__main__':
     chromedriver_autoinstaller.install(cwd=True)
     options = Options()
     options.add_argument('--headless')
-    options.add_argument('--no-sandbox')
-    options.add_argument('--disable-gpu')
-    options.add_argument('--disable-extensions')
+    # options.add_argument('--no-sandbox')
+    # options.add_argument('--disable-gpu')
+    # options.add_argument('--disable-extensions')
     # options.add_experimental_option("excludeSwitches", ["enable-logging"])
 
     driver = webdriver.Chrome(options=options)
