473a2b801054d4cc54fea67c8b6eeced99070c6c
28drinks <28kadsen@gmail.com>
2022-11-11 17:49:30 +0100

    unrevealed new table

--- my_project/routes/reveal.py
+++ my_project/routes/reveal.py
@@ -15,4 +15,40 @@ def whats_left():
 
     trait_types = ["Background","Eyes","Teeth","Body"]
 
-    return render_template("reveal.html",pagetitle="un-revealed", trait_types=trait_types,result=result)
+    total_left = 0
+    total_eye_worth = 0
+    total_teeth_worth = 0
+    total_body_worth = 0
+    total_background_worth = 0
+    x = 0
+    for e in result["Eyes"]:
+        x += 1
+        left = result["Eyes"].get(e)[2]
+        total_left += left
+
+        eye_value = result["Eyes"].get(e)[3]
+        eye_worth = left * eye_value
+        total_eye_worth += eye_worth
+
+    for b in result["Background"]:
+        background_value = result["Background"].get(b)[3]
+        left = result["Background"].get(b)[2]
+        background_worth = left * background_value
+        total_background_worth += background_worth
+
+    for t in result["Teeth"]:
+        teeth_value = result["Teeth"].get(t)[3]
+        left = result["Teeth"].get(t)[2]
+        teeth_worth = left * teeth_value
+        total_teeth_worth += teeth_worth
+
+
+    for bo in result["Body"]:
+        body_value = result["Body"].get(bo)[3]
+        left = result["Body"].get(bo)[2]
+        body_worth = left * body_value
+        total_body_worth += body_worth
+
+    print(total_left,total_background_worth,total_eye_worth,total_teeth_worth,total_body_worth)
+
+    return render_template("reveal.html",pagetitle="un-revealed", trait_types=trait_types,result=result,total_background_worth=total_background_worth,total_body_worth=total_body_worth,total_eye_worth=total_eye_worth,total_teeth_worth=total_teeth_worth,total_left=total_left)
